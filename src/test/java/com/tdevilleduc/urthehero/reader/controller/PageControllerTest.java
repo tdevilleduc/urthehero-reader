7838c60a29be61f1d1aecac3a848efd2cf14b046
Thomas Deville-Duc <devilleduc.thomas@gmail.com>
2020-01-15 20:23:21 +0100

    add mysql database

---
+++ src/test/java/com/tdevilleduc/urthehero/reader/controller/PageControllerTest.java
@@ -0,0 +1,21 @@
+package com.tdevilleduc.urthehero.reader.controller;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class PageControllerTest  {
+
+    private static final String uriController = "/api/page";
+    @Test
+    public void testGetPageById() {
+        given()
+                .when().get(uriController + "/1")
+                .then()
+                .statusCode(200)
+                .body(is("hello"));
+    }
+}
