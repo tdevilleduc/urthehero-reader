93fdf50205e819250964b463f8688b130effb4bd
Thomas Deville-Duc <thomas.deville-duc-prestataire@laposte.fr>
2020-01-20 17:27:53 +0100

    add sql imports

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,8 @@
+-- personId, displayName, login, email
+INSERT INTO person VALUES(1, 'Thomas Deville-Duc', 'thomas@gmail.com', 'tdevilleduc', 'password');
+INSERT INTO person VALUES(2, 'Marion Gianesini', 'marion@gmail.com', 'mgianesini', 'password');
+INSERT INTO person VALUES(3, 'Nicolas Danet', 'nicolas@gmail.com', 'ndanet', 'password');
 -- pageId, image, text
-DROP TABLE IF EXISTS page;
-CREATE TABLE page(id INT PRIMARY KEY NOT NULL, image VARCHAR(100), text VARCHAR(255));
 INSERT INTO page(id, image, text) VALUES(1, 'image3', 'Ulysse');
 INSERT INTO page(id, image, text) VALUES(2, 'image3', 'Dès');
 INSERT INTO page(id, image, text) VALUES(3, 'image3', 'Le roman');
@@ -9,3 +11,21 @@ INSERT INTO page(id, image, text) VALUES(5, 'image3', 'Le roman est notamment c�
 INSERT INTO page(id, image, text) VALUES(6, 'image3', 'Toutefois');
 INSERT INTO page(id, image, text) VALUES(7, 'image3', 'Madame Bovary');
 INSERT INTO page(id, image, text) VALUES(8, 'image2', 'En plein centre');
+-- storyId, authorId, firstPageId, title
+INSERT INTO story(storyId, authorId, firstPageId, title, detailedText, image) VALUES(1, 1, 1, 'Ulysse', 'blablabla Ulysse prenons un texte long pour décrire lhistoire', 'imageUlysse');
+INSERT INTO story(storyId, authorId, firstPageId, title, detailedText, image) VALUES(2, 2, 4, 'Voyage au bout de la nuit', 'bliblibli voyage voyage !!', 'imageVoyage');
+INSERT INTO story(storyId, authorId, firstPageId, title, detailedText, image) VALUES(3, 3, 7, 'Madame Bovary', 'blablabla Bovary', 'imageBovary');
+-- progressionId, pageId, personId, storyId
+INSERT INTO progression VALUES(1, 3, 1, 2);
+INSERT INTO progression VALUES(2, 2, 2, 1);
+INSERT INTO progression VALUES(3, 2, 1, 1);
+INSERT INTO progression VALUES(5, 6, 3, 2);
+INSERT INTO progression VALUES(6, 7, 3, 3);
+INSERT INTO progression VALUES(7, 2, 2, 3);
+INSERT INTO progression VALUES(8, 12, 3, 1);
+-- nextPageId, destinationPageId, pageId, position, text
+INSERT INTO next_page VALUES(1, 2, 1, 0, 'gauche');
+INSERT INTO next_page VALUES(2, 3, 1, 1, 'droite');
+INSERT INTO next_page VALUES(3, 8, 1, 2, 'centre');
+INSERT INTO next_page VALUES(4, 5, 4, 0, 'porte de gauche');
+INSERT INTO next_page VALUES(5, 6, 4, 1, 'porte de droite');
