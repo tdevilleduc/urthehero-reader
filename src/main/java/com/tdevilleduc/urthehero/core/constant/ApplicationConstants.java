2dd8c09ce1435589fa525a9de45f222fa795283b
Thomas Deville-Duc <thomas.deville-duc-prestataire@laposte.fr>
2020-01-29 10:47:10 +0100

    add helloController with service from core lib

--- src/main/java/com/tdevilleduc/urthehero/core/constant/ApplicationConstants.java
+++ src/main/java/com/tdevilleduc/urthehero/core/constant/ApplicationConstants.java
@@ -6,6 +6,7 @@ public final class ApplicationConstants {
     public static final String CHECK_PAGEID_PARAMETER_MANDATORY = "The pageId parameter is mandatory !";
     public static final String CHECK_PERSONID_PARAMETER_MANDATORY = "The personId parameter is mandatory !";
     public static final String CHECK_STORYID_PARAMETER_MANDATORY = "The storyId parameter is mandatory !";
+    public static final String CHECK_NAME_PARAMETER_MANDATORY = "The name parameter is mandatory !";
     public static final String ERROR_MESSAGE_PAGE_DOESNOT_EXIST = "La page avec l'id {} n'existe pas";
     public static final String ERROR_MESSAGE_PERSON_DOESNOT_EXIST = "La personne avec l'id {} n'existe pas";
     public static final String ERROR_MESSAGE_STORY_DOESNOT_EXIST = "L'histoire avec l'id {} n'existe pas";
---
+++ src/main/java/com/tdevilleduc/urthehero/reader/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.tdevilleduc.urthehero.reader.controller;
+
+import com.tdevilleduc.urthehero.core.service.HelloService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+
+import static com.tdevilleduc.urthehero.core.constant.ApplicationConstants.CHECK_NAME_PARAMETER_MANDATORY;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping(value = "/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody
+    ResponseEntity<String> hello(@PathVariable(name = "name") String name) {
+        Assert.notNull(name, CHECK_NAME_PARAMETER_MANDATORY);
+        return ResponseEntity.ok(helloService.hello(name));
+    }
+}
